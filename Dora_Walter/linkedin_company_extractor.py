3a177ca5e25de256d9b4f94204221f278bc773d4
Tasneem Rangwala <tasneem.rangwala05@gmail.com>
2017-12-12 07:51:20 +0530

    commit

--- Dora_Walter/linkedin_company_extractor.py
+++ Dora_Walter/linkedin_company_extractor.py
@@ -18,54 +18,102 @@ chromeOptions.add_argument("--no-sandbox")
 #chromeOptions.add_extension('anticaptcha.crx')
 chromeOptions.add_argument("--disable-setuid-sandbox")
 driver = webdriver.Chrome('./chromedriver.exe',chrome_options=chromeOptions)
+driver1 = webdriver.Chrome('./chromedriver.exe',chrome_options=chromeOptions)
 
 driver.get('https://www.linkedin.com/sales/')
+driver1.get('https://www.linkedin.com/sales/')
 
 
 
 driver.find_element_by_id('session_key-login').send_keys('kumar.jan08@gmail.com')
 driver.find_element_by_id('session_password-login').send_keys('sindhu007')
-
 driver.find_element_by_id('btn-primary').click()
+
+
+driver1.find_element_by_id('session_key-login').send_keys('Tasneem.rangwala05@gmail.com')
+driver1.find_element_by_id('session_password-login').send_keys('muidsa1!2@')
+driver1.find_element_by_id('btn-primary').click()
 
 time.sleep(10)
 output_f = open('linkedin_sales_nav_output','w',encoding='utf-8')
+iiiii=0
 for comp_id in open('linkedin_sales_nav_input').read().split('\n'):
-    driver.get('https://www.linkedin.com/sales/accounts/insights?companyId='+str(comp_id))
-    time.sleep(5)
-    output = str(comp_id)+'\t'
-    
-    try:
-        output += driver.find_element_by_css_selector('.acct-name').text+'\t'
-    except:
-        output += '\t'
+    if iiiii%2==0:
+        driver.get('https://www.linkedin.com/sales/accounts/insights?companyId='+str(comp_id))
+        time.sleep(5)
+        output = str(comp_id)+'\t'
         
-    try:
-        output += driver.find_element_by_css_selector('.location.detail').text+'\t'
-    except Exception as e:
-        #print (e)
-        output += '\t'
+        try:
+            output += driver.find_element_by_css_selector('.acct-name').text+'\t'
+        except:
+            output += '\t'
+            
+        try:
+            output += driver.find_element_by_css_selector('.location.detail').text+'\t'
+        except Exception as e:
+            #print (e)
+            output += '\t'
+            
+        try:
+            output += driver.find_element_by_css_selector('.size.detail').text+'\t'
+        except:
+            output += '\t'
         
-    try:
-        output += driver.find_element_by_css_selector('.size.detail').text+'\t'
-    except:
-        output += '\t'
-    
-    try:
-        output += driver.find_element_by_css_selector('.industry.detail').text+'\t'
-    except:
-        output += '\t'    
-    
-    try:
-        output += driver.find_element_by_css_selector('.account-term-desc.addr-term-desc').text+'\t'
-    except:
-        output += '\t'
-    
-    try:
-        output += driver.find_element_by_css_selector('.account-term:nth-child(3) p a').text+'\n'
-    except Exception as e:
-        output += '\n'
-    print (output)
-    output_f.write(output)
-    output_f.flush()
-    time.sleep(25)
+        try:
+            output += driver.find_element_by_css_selector('.industry.detail').text+'\t'
+        except:
+            output += '\t'    
+        
+        try:
+            output += driver.find_element_by_css_selector('.account-term-desc.addr-term-desc').text+'\t'
+        except:
+            output += '\t'
+        
+        try:
+            output += driver.find_element_by_css_selector('.account-term:nth-child(3) p a').text+'\n'
+        except Exception as e:
+            output += '\n'
+        print (output)
+        output_f.write(output)
+        output_f.flush()
+        time.sleep(10)
+    else:
+        driver1.get('https://www.linkedin.com/sales/accounts/insights?companyId='+str(comp_id))
+        time.sleep(5)
+        output = str(comp_id)+'\t'
+        
+        try:
+            output += driver1.find_element_by_css_selector('.acct-name').text+'\t'
+        except:
+            output += '\t'
+            
+        try:
+            output += driver1.find_element_by_css_selector('.location.detail').text+'\t'
+        except Exception as e:
+            #print (e)
+            output += '\t'
+            
+        try:
+            output += driver1.find_element_by_css_selector('.size.detail').text+'\t'
+        except:
+            output += '\t'
+        
+        try:
+            output += driver1.find_element_by_css_selector('.industry.detail').text+'\t'
+        except:
+            output += '\t'    
+        
+        try:
+            output += driver1.find_element_by_css_selector('.account-term-desc.addr-term-desc').text+'\t'
+        except:
+            output += '\t'
+        
+        try:
+            output += driver1.find_element_by_css_selector('.account-term:nth-child(3) p a').text+'\n'
+        except Exception as e:
+            output += '\n'
+        print (output)
+        output_f.write(output)
+        output_f.flush()
+        time.sleep(10)
+    iiiii+=1
